4c5057d75b8215f347f9bb6a301d5bf85fa6bd2b
Haneul Cho <stuykr@naver.com>
2021-09-06 02:07:09 +0900

    add:  add custom theme

---
+++ .storybook/theme-custom.js
@@ -0,0 +1,27 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+
+  colorPrimary: '#aaaaaa',
+  colorSecondary: '#3c3c3c',
+
+  appBg: '#1f1f1f',
+  appContentBg: 'white',
+  appBorderColor: '#eeeeee',
+  appBorderRadius: 4,
+
+  textColor: '#aaaaaa',
+
+  barTextColor: '#555555',
+  barSelectedColor: '#1f1f1f',
+  barBg: '#f8f8f8',
+
+  inputBg: 'f0f0f0',
+  inputBorder: '#cccccc',
+  inputTextColor: '#555555',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Design System',
+  brandUrl: 'https://example.com',
+});
